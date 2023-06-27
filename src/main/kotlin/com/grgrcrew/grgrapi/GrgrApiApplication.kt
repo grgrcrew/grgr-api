143fc2874a061b59b6757bdb61c77c72f90c202b
hanbee <sonhanbi1002@gmail.com>
2023-06-27 17:44:52 +0900

    init

---
+++ src/main/kotlin/com/grgrcrew/grgrapi/GrgrApiApplication.kt
@@ -0,0 +1,11 @@
+package com.grgrcrew.grgrapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrgrApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<GrgrApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/grgrcrew/grgrapi/GrgrApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.grgrcrew.grgrapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrgrApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
